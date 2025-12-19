9cf7818af048ccb4fd48e189422cf464eabea2f6
Stella-055 <stellaongayo243@gmail.com>
2025-12-19 16:53:06 +0300

    added sidebar

--- app/(routes)/dashboard/_components/welcomeBanner.tsx
+++ app/(routes)/dashboard/_components/welcomeBanner.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const WelcomeBanner = () => {
@@ -5,6 +6,7 @@ const WelcomeBanner = () => {
     <div className='p-5 bg-gradient-to-r from-[#BE575F] via-[#A33833] to-[#AC7606] text-white rounded-lg mb-6'>
       <h2 className='font-bold text-2xl'>Ai Career Coach</h2>
       <p > Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto consequatur vero voluptatum consectetur earum alias accusamus, mollitia quo nisi illo facere fugit quibusdam laborum ipsum molestiae atque blanditiis magnam eveniet.</p>
+      <Button className='text-black' variant={'outline'}>Get Started</Button>
     </div>
   )
 }
