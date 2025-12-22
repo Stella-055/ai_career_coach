3fc24ce1028ef27e0b721b28700aa9614c5bb9b9
Stella-055 <stellaongayo243@gmail.com>
2025-12-22 18:04:29 +0300

    add billing component

--- app/(routes)/dashboard/billing/page.tsx
+++ app/(routes)/dashboard/billing/page.tsx
@@ -2,8 +2,22 @@ import { PricingTable } from '@clerk/nextjs'
 
 export default function PricingPage() {
   return (
-    <div style={{ maxWidth: '800px', margin: '0 auto', padding: '0 1rem' }}>
+    <>
+     <div className="w-full flex items-center justify-between px-4 md:px-14 py-1 font-medium text-sm text-white text-center bg-gradient-to-r from-blue-500 to-purple-600 mb-6">
+            <p>Get 20% OFF on Your First Order!</p>
+            <div className="flex items-center space-x-6">
+                <button type="button" className="font-normal text-gray-800 bg-white px-7 py-2 rounded-full">Claim Offer</button>
+                <button type="button">
+                    <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <rect y="12.532" width="17.498" height="2.1" rx="1.05" transform="rotate(-45.74 0 12.532)" fill="#fff"/>
+                        <rect x="12.533" y="13.915" width="17.498" height="2.1" rx="1.05" transform="rotate(-135.74 12.533 13.915)" fill="#fff"/>
+                    </svg>
+                </button>
+            </div>
+        </div>
+   
+    <div style={{  margin: '0 auto', padding: '0 1rem' }}>
       <PricingTable />
-    </div>
+    </div>  </>
   )
 }
