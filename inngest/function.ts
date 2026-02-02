d99bce8d42d6a42901c23387129590fee7d6f597
Stella-055 <stellaongayo243@gmail.com>
2026-02-02 15:14:33 +0300

    premium section

---
+++ inngest/function.ts
@@ -0,0 +1,23 @@
+import { inngest } from "./client";
+import {
+    createNetwork,
+    createAgent,
+    openai,
+    anthropic,
+  } from "@inngest/agent-kit";
+export const helloWorld = inngest.createFunction(
+  { id: "hello-world" },
+  { event: "test/hello.world" },
+  async ({ event, step }) => {
+    await step.sleep("wait-a-moment", "1s");
+    return { message: `Hello ${event.data.email}!` };
+  },
+);
+
+export const AiCareerChatAgent= createAgent({
+    name: "AiCareerChatAgent",
+  system: "You are a helpful, professional AI Career Coach Agent. Your role is to guide users with questions related to careers, including job search advice, interview preparation, resume improvement, skill development, career transitions, and industry trends. Always respond with clarity, encouragement, and actionable advice tailored to the user's needs. If the user asks something unrelated to careers (e.g., topics like health, relationships, coding help, or general trivia), gently inform them that you are a career coach and suggest relevant career-focused questions instead.",
+  model:openai({model:"",
+    apiKey:process.env?.GEMINI_API
+  })
+})
