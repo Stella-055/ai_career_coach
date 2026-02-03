eb8ff1310a7b2a1cc383040fde224c5117b3073c
Stella-055 <stellaongayo243@gmail.com>
2026-02-03 10:19:51 +0300

    add uuid generation for eachsession

--- app/api/history/route.ts
+++ app/api/history/route.ts
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { db } from "@/configs/db";
 import { userHistory } from "@/configs/schema";
 import { currentUser } from "@clerk/nextjs/server";
+import { eq } from "drizzle-orm";
 export async  function POST(req:NextRequest){
     const{content, requestId}= await req.json()
     const user = await  currentUser()
@@ -13,6 +14,21 @@ try {
         useremail:user?.primaryEmailAddress?.emailAddress,
         createdAt:(new Date()).toString()
     })
+    return NextResponse.json(result)
+} catch (error) {
+    return NextResponse.json(error)
+}
+
+}
+
+export async  function PUT(req:NextRequest){
+    const{content,requestId}= await req.json()
+ 
+try {
+    const result= await db.update(userHistory).set({
+        content:content,
+      
+    }).where(eq(userHistory.recordId,requestId))
     return NextResponse.json(result)
 } catch (error) {
     return NextResponse.json(error)
