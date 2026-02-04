e0192bee585ee619df2c05f2a5ef56f26f7fe039
Stella-055 <stellaongayo243@gmail.com>
2026-02-04 13:48:49 +0300

    dialog for resume upload

--- app/(routes)/dashboard/_components/resumeUploadDialog.tsx
+++ app/(routes)/dashboard/_components/resumeUploadDialog.tsx
@@ -10,20 +10,26 @@ import {
     AlertDialogTrigger,
   } from "@/components/ui/alert-dialog"
   import { Button } from "@/components/ui/button"
+import { File, FileIcon, Sparkles } from "lucide-react";
 const ResumeUploadDialog = ({open,setDialogOpen}:{open:boolean;setDialogOpen:any}) => {
   return (
     <AlertDialog open={open} onOpenChange={setDialogOpen}>
     <AlertDialogContent >
       <AlertDialogHeader>
-        <AlertDialogTitle></AlertDialogTitle>
+        <AlertDialogTitle>Upload Resume pdf file</AlertDialogTitle>
         <AlertDialogDescription>
-          This action cannot be undone. This will permanently delete your account
-          from our servers.
+         <div>
+            <label htmlFor="dialogform" className="flex items-center flex-col cursor-pointer justify-center p-7 rounded-xl border border-dashed">
+                <File className="h-10 w-10"/>
+                <h2 className="mt-3">Click here to upload PDF file</h2>
+            </label>
+            <input type="file" id="dialogform" className="opacity-0"/>
+         </div>
         </AlertDialogDescription>
       </AlertDialogHeader>
       <AlertDialogFooter>
         <AlertDialogCancel>Cancel</AlertDialogCancel>
-        <AlertDialogAction>Continue</AlertDialogAction>
+        <AlertDialogAction> <Sparkles/>Upload & analyze</AlertDialogAction>
       </AlertDialogFooter>
     </AlertDialogContent>
   </AlertDialog>
