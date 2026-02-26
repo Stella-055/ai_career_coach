c12590be23adaa2690b49f5f7cc07efb05c21574
Stella-055 <stellaongayo243@gmail.com>
2026-02-26 17:05:41 +0300

    add roadmap gen agent

--- app/(routes)/dashboard/roadmap/[roadmapId]/page.tsx
+++ app/(routes)/dashboard/roadmap/[roadmapId]/page.tsx
@@ -1,8 +1,9 @@
 "use client"
 import axios from 'axios'
-import React from 'react'
+import { Button } from '@/components/ui/button'
 import { useEffect,useState } from 'react'
 import { useParams } from 'next/navigation'
+
 const page = () => {
     const {recordId}= useParams()
    
@@ -21,9 +22,11 @@ const page = () => {
           },[recordId])
   return (
     <div className='grid grid-cols-1 md:grid-cols-3 gap-5'>
-        <div>
+        <div className='border p-5 rounded-lg'>
             <h2 className='font-bold text-2xl'>{airesponse?.roadmapTitle}</h2>
             <p className='mt-3 text-gray-500'> <strong>Description</strong><br />{airesponse?.description}</p>
+            <h2 className='mt-5 font-medium'>Duration: {airesponse?.duration}</h2>
+            <Button className='w-full mt-5'>+create new Roadmap</Button>
         </div>
         <div className='md:grid-cols-2'></div>
     </div>
